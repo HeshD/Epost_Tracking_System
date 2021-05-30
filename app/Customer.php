edadb315f4ec85ff46f0542cad528df07a789d84
Heshan Dilruk <57480748+HeshD@users.noreply.github.com>
2021-05-30 14:23:49 +0530

    first commit

---
+++ app/Customer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Model\Branch;
+
+class Customer extends Authenticatable {
+
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'phone', 'image', 'password', 'status'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    
+}
